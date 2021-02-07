bda3edfac0845ecb98bc12b85a4dc8aabf22c3aa
keert <keert@LAPTOP-GEL9MMCO>
2021-02-07 12:24:43 +0530

    Relational operators

---
+++ javaSelenium/src/edu/learning/RelationalOperators.java
@@ -0,0 +1,21 @@
+package edu.learning;
+
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+
+		int myFirstNum =55;
+		int mySecondNum =55;
+		
+		if(myFirstNum <= mySecondNum)
+		System.out.println(myFirstNum + "<=" + mySecondNum);
+		
+		else if(myFirstNum >= mySecondNum)
+			System.out.println(myFirstNum + ">=" + mySecondNum);
+		
+		else if (myFirstNum == mySecondNum)
+			System.out.println(myFirstNum + "==" + mySecondNum);
+
+	}
+
+}
