65160342a38d98a4375d37a3a7813a6a8b9f7fb9
keert <keert@LAPTOP-GEL9MMCO>
2021-02-07 12:01:27 +0530

    Operator problem

---
+++ javaSelenium/src/edu/learning/Challenge.java
@@ -0,0 +1,31 @@
+package edu.learning;
+
+public class Challenge {
+
+	public static void main(String[] args) {
+	
+		double myFirstDouble = 20.0d;
+		double mySecondDouble = 100.0d;
+		
+		System.out.println("myFirstDouble * mySecondDouble =" + (myFirstDouble * mySecondDouble));
+		
+		double step1Res = 20.0d * 100.0d;
+		System.out.println("step1Res % 20 = " + step1Res % 20);
+		
+		double step3Res = Step1Res + 20.0d;
+		System.out.println("step2Res");
+			
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ javaSelenium/src/edu/learning/Operator.java
@@ -0,0 +1,29 @@
+package edu.learning;
+
+public class Operator {
+
+	public static void main(String[] args) {
+	
+		int myFirstNum =1;
+		int mySecondNum = 4;
+		
+		int result = 1+4;
+		
+		System.out.println(" 1 + 4 = 5" + result);
+
+		System.out.println(" 1 + 4 = 5.25d" + (result + 0.25d));
+		
+		System.out.println(" 1 / 4d = 0.25" + (1/4d));
+		
+		double myFirstDouble = 1d;
+		double mySecondDouble = 4d;
+		
+		system.out.println("1d / 4d = 0.00" + MyFirstDouble % mySecondDouble);
+		
+		myFirstNum +=4;
+		
+		system.out.println("MyFirstNum += 4 -> " + myFirstNum);
+		
+	}
+
+}
