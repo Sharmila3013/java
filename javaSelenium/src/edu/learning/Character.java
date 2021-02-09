4b1e1da314b7ba69a094a3bdb6e8dbb25666883a
keert <keert@LAPTOP-GEL9MMCO>
2021-02-09 10:04:46 +0530

    Result

---
+++ javaSelenium/src/edu/learning/Character.java
@@ -0,0 +1,23 @@
+package edu.learning;
+
+public class Character {
+
+	public static void main(String[] args) {
+		
+		char myFirstchar = 'P';
+		char mySecondChar = 'F';
+		
+		char result = 'P';
+		
+		if(result == 'P')
+		{
+			System.out.println("Pass");
+		}
+		else if(result == 'F')
+		{
+			System.out.println("Fail");
+		}
+		
+	}
+
+}
