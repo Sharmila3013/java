0d33efd85a711417517de366187612ce339c6569
keert <keert@LAPTOP-GEL9MMCO>
2021-02-10 07:40:45 +0530

    Operator

--- javaSelenium/src/edu/learning/Operator.java
+++ javaSelenium/src/edu/learning/Operator.java
@@ -7,22 +7,14 @@ public class Operator {
 		int myFirstNum =1;
 		int mySecondNum = 4;
 		
-		int result = 1+4;
+		int total = myFirstNum + mySecondNum;
 		
-		System.out.println(" 1 + 4 = 5" + result);
+		System.out.println(" 1 + 4 = 5" +  total);
 
-		System.out.println(" 1 + 4 = 5.25d" + (result + 0.25d));
+		System.out.println(" 1 + 4 = 5.25d" +  (total + 0.25d));
 		
-		System.out.println(" 1 / 4d = 0.25" + (1/4d));
-		
-		double myFirstDouble = 1d;
-		double mySecondDouble = 4d;
-		
-		system.out.println("1d / 4d = 0.00" + MyFirstDouble % mySecondDouble);
-		
-		myFirstNum +=4;
-		
-		system.out.println("MyFirstNum += 4 -> " + myFirstNum);
+		System.out.println(" 1 / 4d = 0.25" +  (1/4d));
+	
 		
 	}
 
