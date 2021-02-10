f951b00a40c1329c834c5c299e4da717ec5fdbf4
keert <keert@LAPTOP-GEL9MMCO>
2021-02-10 08:03:32 +0530

    Logical Operators

--- javaSelenium/src/edu/learning/LogicalOperators.java
+++ javaSelenium/src/edu/learning/LogicalOperators.java
@@ -4,24 +4,27 @@ public class LogicalOperators {
 
 	public static void main(String[] args) {
 	
-		int myMathScore = 22;
-		int mySciScore = 10;
+		int myMathScore = 92;
+		int mySciScore = 22;
 		
-		int mathplussci = myMathScore+mySciScore;
-			
-		boolean Distinction = ((myMathScore>80) && (mySciScore>80));
+		int mathplussci = myMathScore + mySciScore;
+		System.out.println("Maths + Science score = " + mathplussci);	
 		
-		if( Distinction || (mathplussci >= 160))
+		boolean Distinction = (((myMathScore>=80) && (mySciScore>=80)) || (mathplussci >= 160));
+		boolean FirstClass = ((mathplussci < 120) || (mathplussci > 35)); 
+		boolean NotGood = ((myMathScore<35) && (mySciScore<35));
+		
+		if( Distinction == true)
 		{
 			System.out.println("Distinction");			
 		}
 		
-		else if ((mathplussci < 120) || (mathplussci < 35))
+		else if (FirstClass == true)
 		{
 			System.out.println("First Class");
 		}
 		
-		else if (mathplussci < 160) || (mathplussci > 120)
+		else if (NotGood == true)
 		{
 			System.out.println("Not Good");
 		}
