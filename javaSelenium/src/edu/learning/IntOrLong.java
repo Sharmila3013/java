6fe77da0908b0d85201d8254802215c0dd3e0e2b
keert <keert@LAPTOP-GEL9MMCO>
2021-02-13 07:50:02 +0530

    IntOrLong

---
+++ javaSelenium/src/edu/learning/IntOrLong.java
@@ -0,0 +1,25 @@
+package edu.learning;
+
+public class IntOrLong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int myNum1 = -2147483647;
+		
+		
+		//int intOrLong = ((-2147483648<<myNum1)||(myNum1>>2147483647));
+		
+		if((myNum1 <=2147483647)||(myNum1>=-2147483648))
+		{
+			System.out.println(myNum1 + "is an integer");
+		}
+		
+		else 
+		{
+			System.out.println(myNum1+ "is long");
+		}
+			
+	}
+
+}
